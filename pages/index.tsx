f8580386c61dbea5c65ff3a0011ad55eb575efcf
Achalogy <achalogy@gmail.com>
2023-04-16 19:48:51 -0500

    ✨ Improve Landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 export default () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl">
-        NextJS Boilerplate
+    <div className="flex flex-col gap-2 items-center justify-center h-screen p-8 text-center">
+      <h1 className="text-6xl font-semibold text-slate-800">
+        NextJS <span className="text-indigo-500 font-semibold">Boilerplate</span>
       </h1>
+      <p className="text-xl">Created by <a href="https://achalogy.dev/" className="text-sky-500">Achalogy</a></p>
     </div>
   )
 }
